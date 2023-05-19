8025a3ae2600bbba8beb7ed9c4f9225c4abaec52
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-19 20:25:31 +0300

    Triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,19 @@
 class Triangle
-  def initialize(side1, side2, side3)
+  class TriangleError < StandardError
+  end
+
+  def initialize(side1,side2,side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      raise TriangleError, "Sides must have positive length"
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      raise TriangleError, "Not a valid triangle"
+    elsif @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
